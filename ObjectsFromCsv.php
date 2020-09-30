fe738480cc20b6333534bda3eaa838ea99bee256
Dan <sophismofficial@gmail.com>
2020-09-30 21:54:00 +0100

    moved objects from csv class to separate file

---
+++ ObjectsFromCsv.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types = 1);
+
+include 'Validator.php';
+
+class ObjectsFromCsv
+{
+	protected object $validator;
+
+	protected array $expectedCsvHeaders;
+
+
+	public function __construct()
+	{
+		$this->validator = new Validator();
+	}
+
+
+	public function run(string $className): array
+	{
+		include $className . ".php";
+
+		$csvFilename = $className . "s.csv";
+
+		$handle = fopen($csvFilename, 'r');
+
+		$actualCsvHeaders = fgetcsv($handle);
+
+		$this->setExpectedCsvHeadersFromClassConstructor($className);
+
+		$this->validator->validateCsvHeaders($this->expectedCsvHeaders, $actualCsvHeaders);
+
+		$results = [];
+
+		$count = 0;
+
+		while (!feof($handle)){
+			$row = fgetcsv($handle);
+
+			if (!$row){
+				continue;
+			}
+
+			$results[$count] = new Card(...$row);
+
+			++$count;
+		}
+
+		fclose($handle);
+
+		return $results;
+	}
+
+
+	protected function setExpectedCsvHeadersFromClassConstructor(string $className): void
+	{
+		$this->expectedCsvHeaders = [];
+
+		$reflectionClass = new ReflectionClass($className);
+
+		$classConstructorParams = $reflectionClass->getConstructor()->getParameters();
+
+		$count = 0;
+
+		foreach($classConstructorParams as $param){
+
+			$this->validator->validateParamTypeString($param);
+
+			$this->expectedCsvHeaders[$count] = $param->name;
+
+			++$count;
+
+		}
+
+		return;
+	}
+}
--- index.php
+++ index.php
@@ -2,81 +2,10 @@
 
 declare(strict_types = 1);
 
-include 'Validator.php';
+include 'ObjectsFromCsv.php';
 
-class CsvToObjectArray
-{
-	protected object $validator;
+$objectsFromCsv = new ObjectsFromCsv();
 
-	protected array $expectedCsvHeaders;
+$results = $objectsFromCsv->run('Card');
 
-
-	public function __construct()
-	{
-		$this->validator = new Validator();
-	}
-
-
-	public function read(string $className): array
-	{
-		include $className . ".php";
-
-		$csvFilename = $className . "s.csv";
-
-		$handle = fopen($csvFilename, 'r');
-
-		$actualCsvHeaders = fgetcsv($handle);
-
-		$this->setExpectedCsvHeadersFromClassConstructor($className);
-
-		$this->validator->validateCsvHeaders($this->expectedCsvHeaders, $actualCsvHeaders);
-
-		$results = [];
-
-		$count = 0;
-
-		while (!feof($handle)){
-			$row = fgetcsv($handle);
-
-			if (!$row){
-				continue;
-			}
-
-			$results[$count] = new Card(...$row);
-
-			++$count;
-		}
-
-		fclose($handle);
-
-		return $results;
-	}
-
-
-	protected function setExpectedCsvHeadersFromClassConstructor(string $className): void
-	{
-		$this->expectedCsvHeaders = [];
-
-		$reflectionClass = new ReflectionClass($className);
-
-		$classConstructorParams = $reflectionClass->getConstructor()->getParameters();
-
-		$count = 0;
-
-		foreach($classConstructorParams as $param){
-
-			$this->validator->validateParamTypeString($param);
-
-			$this->expectedCsvHeaders[$count] = $param->name;
-
-			++$count;
-
-		}
-
-		return;
-	}
-}
-
-$csvToObjectArray = new CsvToObjectArray();
-$results = $csvToObjectArray->read('Card');
 var_dump($results);
