0bfbda8faaa2947f9f5e308fbd0aa6266c131754
coderman17 <daniel.mathews@hotmail.co.uk>
2020-10-06 22:27:47 +0100

    spaces to tabs

--- src/.env.example.php
+++ src/.env.example.php
@@ -5,9 +5,9 @@ declare(strict_types = 1);
 namespace Flashcard;
 
 $variables = [
-    'APP_DOMAIN' => "localhost"
+	'APP_DOMAIN' => "localhost"
 ];
 
 foreach ($variables as $key => $value) {
-    putenv("$key=$value");
+	putenv("$key=$value");
 }
