2e4d3d2ec3b620e59e0dbe62eb5b89857ddd6e61
Dan <sophismofficial@gmail.com>
2020-10-06 22:20:48 +0100

    added a redirect object and hooked up to router section in App

---
+++ src/Redirect.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Flashcard;
+
+class Redirect
+{
+	public function __construct()
+	{
+		if (null == getenv('APP_DOMAIN')){
+			echo 'no app_domain registered in env file for redirect';
+			exit();
+		} else {
+			$this->domain = getenv('APP_DOMAIN');
+		}
+	}
+    public function go($uri)
+    {
+        if ($this->domain == "localhost"){
+            header("Location: http://localhost:" . $_SERVER[SERVER_PORT] . $uri);
+        } else {
+            header("Location: http://" . $this->domain . $uri);
+        }
+    }
+}
